b5c1af84d9cfe0deed762922ecea0fa8f20ad691
Hari krishnan <143381736+HariOp6238@users.noreply.github.com>
2023-11-07 10:29:05 +0530

    firstcommit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:sos/view/splashscreen/splashscreen.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Splashscreen(),
+    );
+  }
+}
---
+++ lib/view/splashscreen/splashscreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return Scaffold(
+      backgroundColor: Colors.amber,
+    );
+  }
+}
