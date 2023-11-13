d5ff1a1cc100385466305ad1f2836e143769e4a2
Hari krishnan <143381736+HariOp6238@users.noreply.github.com>
2023-11-13 21:55:51 +0530

    notification added

---
+++ lib/view/notification/noti.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class noti extends StatefulWidget {
+  const noti({super.key});
+
+  @override
+  State<noti> createState() => _notiState();
+}
+
+class _notiState extends State<noti> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red.shade700,
+      ),
+      body: Center(
+        child: Column(
+          children: [Text('No new Notifications')],
+        ),
+      ),
+    );
+  }
+}
