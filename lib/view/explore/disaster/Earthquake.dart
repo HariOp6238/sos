e7e38cf44ac0b3ac0ef2c76cca5eda610b822500
Hari krishnan <143381736+HariOp6238@users.noreply.github.com>
2023-11-16 01:01:19 +0530

    edited

---
+++ lib/view/explore/disaster/Earthquake.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class earthquake extends StatelessWidget {
+  const earthquake({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
